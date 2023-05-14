cf26f26d5ee7bc86a4ee9f67c1951ae3065c421a
datluu-dev <tad.louis2810@gmail.com>
2023-05-14 15:48:42 +1000

    get refresh token

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from api import views
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
+    # path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/login', views.Login.as_view()),
     path('auth/customer/signup', views.CustomerSignUp.as_view()),
     path('auth/employer/signup', views.createEmployerAccount),
